21edb9322a21cf3ecd5a3504f58de531aff2cb77
FifthTundraG <117035030+FifthTundraG@users.noreply.github.com>
2025-07-01 21:18:41 -0400

    Initial commit

---
+++ src/main/java/io/github/fifthtundrag/no_death_only_dogg/NoDeathOnlyDogg.java
@@ -0,0 +1,10 @@
+package io.github.fifthtundrag.no_death_only_dogg;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NoDeathOnlyDogg implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
